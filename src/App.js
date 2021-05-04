8073a2320b9922b3e15f8b92f889be87aec66cdc
Ninu <ninuvv032@gmail.com>
2021-05-04 21:16:37 +0300

    homepageloaded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from 'react';
+import React from "react";
 import Navbar from "./components/navbar";
 import Myfooter from "./components/footer";
 // import Mycarousel from "./components/carousel";
@@ -11,38 +11,38 @@ import Skills from "./pages/skills/Skills";
 import Timeline from "./pages/experiences/Timeline";
 import Projects from "./pages/projects/projects";
 
-import {  Route } from "react-router-dom";
-import { HashRouter as Router } from 'react-router-dom'
+import { Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Contact from "./pages/contact/contact";
 import Particles from "react-particles-js";
 
 const App = () => {
   return (
-   <>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Navbar />
-        <Particles
-          // params={{
-          //     "particles": {
-          //         "number":{"value":100},
-          //         "size":{"value":3}
+    <>
+      <HashRouter basename="/">
+        <Router>
+          <Navbar />
+          <Particles
+            // params={{
+            //     "particles": {
+            //         "number":{"value":100},
+            //         "size":{"value":3}
 
-          //     }
-          // }}
-          height="100vh"
-        />
+            //     }
+            // }}
+            height="100vh"
+          />
 
-        <Route path="/" exact component={Mytitlemsg} />
+          <Route path="/" exact component={Mytitlemsg} />
           <Route path="/about" component={MyAbout} />
           <Route path="/skills" component={Skills} />
           <Route path="/experience" component={Timeline} />
           <Route path="/projects" component={Projects} />
           <Route path="/contact" component={Contact} />
-   
-      </Router>
+        </Router>
+      </HashRouter>
       <Myfooter />
     </>
-
   );
 };
 
