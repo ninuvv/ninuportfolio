d89e15d36c01fa9ab7b14197f3b6d7372048b06c
Ninu <ninuvv032@gmail.com>
2021-05-05 01:39:27 +0300

    home nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,13 +9,13 @@ const navbar = () => {
   return (
     <Navbar fixed='top' expand="lg" bg="dark" variant="dark"  collapseOnSelect>
      
-      <Navbar.Brand href="#">
+      <Navbar.Brand href="#/">
          <img className="logo" src={logo} alt=""/>
            </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="#" className="main-nav"  activeClassName=" main-nav-active">Home</Nav.Link>
+          <Nav.Link href="#/" className="main-nav"  activeClassName=" main-nav-active">Home</Nav.Link>
           <Nav.Link href="#about" className="main-nav"  activeClassName=" main-nav-active">About</Nav.Link>
           <Nav.Link href="#skills" className="main-nav"  activeClassName=" main-nav-active">Skills</Nav.Link>
           <Nav.Link href="#experience" className="main-nav"  activeClassName=" main-nav-active">Experience</Nav.Link>
