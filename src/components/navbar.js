53932ca7f6d40fc00a2cb497196d7c92e212715a
Ninu <ninuvv032@gmail.com>
2021-05-05 01:35:29 +0300

    home nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,19 +3,19 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import logo from "../../src/logo1.png";
 import "../../src/App.css";
-import { NavLink } from "react-router-dom";
+
 
 const navbar = () => {
   return (
     <Navbar fixed='top' expand="lg" bg="dark" variant="dark"  collapseOnSelect>
      
-      <Navbar.Brand href="/">
+      <Navbar.Brand href="#">
          <img className="logo" src={logo} alt=""/>
            </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="/" className="main-nav"  activeClassName=" main-nav-active">Home</Nav.Link>
+          <Nav.Link href="#" className="main-nav"  activeClassName=" main-nav-active">Home</Nav.Link>
           <Nav.Link href="#about" className="main-nav"  activeClassName=" main-nav-active">About</Nav.Link>
           <Nav.Link href="#skills" className="main-nav"  activeClassName=" main-nav-active">Skills</Nav.Link>
           <Nav.Link href="#experience" className="main-nav"  activeClassName=" main-nav-active">Experience</Nav.Link>
